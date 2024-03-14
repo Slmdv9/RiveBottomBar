bbd9b952b6b546399b8ebfa2c4aa3d526531759a
Samuel <samuel9lmdv@gmail.com>
2024-03-14 16:35:13 +0100

    Fix. Assets and Scaffold in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,24 @@ class _BottonNavWithAnimatedIconsState
     extends State<BottonNavWithAnimatedIcons> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: SafeArea(
-          child: Container(
-        height: 56,
-        padding: const EdgeInsets.all(12),
-        margin: const EdgeInsets.symmetric(horizontal: 24),
-        decoration: BoxDecoration(
-            color: bottonNavBgColor.withOpacity(0.8),
-            borderRadius: const BorderRadius.all(Radius.circular(24)),
-            boxShadow: [
-              BoxShadow(
-                  color: bottonNavBgColor.withOpacity(0.3),
-                  offset: const Offset(0, 20),
-                  blurRadius: 20)
-            ]),
-      )),
+    return MaterialApp(
+      home: Scaffold(
+        bottomNavigationBar: SafeArea(
+            child: Container(
+          height: 56,
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+              color: bottonNavBgColor.withOpacity(0.8),
+              borderRadius: const BorderRadius.all(Radius.circular(24)),
+              boxShadow: [
+                BoxShadow(
+                    color: bottonNavBgColor.withOpacity(0.3),
+                    offset: const Offset(0, 20),
+                    blurRadius: 20)
+              ]),
+        )),
+      ),
     );
   }
 }
