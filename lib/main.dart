793e47a9a6edaafcf7a3f881fb6d5e58d6fba6c1
Samuel <samuel9lmdv@gmail.com>
2024-03-17 17:22:33 +0100

    Created the Animated Bar

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,14 @@ class _BottonNavWithAnimatedIconsState
   List<SMIBool> riveIconInputs = [];
   List<StateMachineController> controllers = [];
   int selectedNavIndex = 0;
+  List<String> pages = ["Chat", "Search", "History", "notification", "Profile"];
+
+  void animateTheIcon(int index) {
+    riveIconInputs[index].change(true);
+    Future.delayed(const Duration(seconds: 1), () {
+      riveIconInputs[index].change(false);
+    });
+  }
 
   void riveOnInit(Artboard artboard, {required String stateMachineName}) {
     StateMachineController? controller =
@@ -33,9 +41,20 @@ class _BottonNavWithAnimatedIconsState
   }
 
   @override
+  void dispose() {
+    for (var controller in controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Center(
+          child: Text(pages[selectedNavIndex]),
+        ),
         bottomNavigationBar: SafeArea(
             child: Container(
           height: 56,
@@ -52,22 +71,59 @@ class _BottonNavWithAnimatedIconsState
               ]),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: List.generate(
-                bottomNavItems.length,
-                (index) => SizedBox(
+            children: List.generate(bottomNavItems.length, (index) {
+              final riveIcon = bottomNavItems[index];
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedNavIndex = index;
+                  });
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    AnimatedBar(isActive: selectedNavIndex == index),
+                    SizedBox(
                       height: 36,
                       width: 36,
                       child: RiveAnimation.asset(
                         bottomNavItems[index].src,
                         artboard: bottomNavItems[index].artboard,
                         onInit: (artboard) {
-                          //riveOnInit(artboard, stateMachineName: riveIcon.stateMachineName);
+                          riveOnInit(artboard,
+                              stateMachineName: riveIcon.stateMachineName);
                         },
                       ),
-                    )),
+                    ),
+                  ],
+                ),
+              );
+            }),
           ),
         )),
       ),
+    );
+  }
+}
+
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    super.key,
+    required this.isActive,
+  });
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.only(bottom: 2),
+      height: 4,
+      width: isActive ? 20 : 0,
+      decoration: const BoxDecoration(
+          color: Color(0xFF81B4FF),
+          borderRadius: BorderRadius.all(Radius.circular(12))),
     );
   }
 }
